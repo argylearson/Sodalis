6b45a0f90a0afe41c633d54e8b78a05b4290ea17
Richard Lemke <rlemke@indiana.edu>
2020-04-21 20:15:23 -0400

    preventDefault saves lives

--- BasicFrontEnd/Components/loginForm/loginForm.js
+++ BasicFrontEnd/Components/loginForm/loginForm.js
@@ -10,22 +10,67 @@ class LoginForm extends HTMLFormElement {
         css.href = "Components/loginForm/loginForm.css";
         document.head.appendChild(css);
 
-
-        //this.className = "login-form";
-        this.action = "https://localhost:5001/api/authentication/login";
-        this.method = "post";
+        this.onsubmit = (event) => {
+            event.preventDefault();
+            this.loginFetch();
+        };
         this.innerHTML = "" +
-            "<div class='login-form'>" +
-                "<h3 class='login-text'>Login</h3>" +
-                "<div class='email-box' style='height:30px'>" +
-                    "<label for='emailAddress' style='width:30px'><img src='Assets/Icons/email.png' height='100%'></label>" +
-                    "<input type='email' id='emailAddress' name='emailAddress' required='required'>" +
+            "<div class='login-form' id='login-form'>" +
+                "<div class='email-box'>" +
+                    "<label for='emailAddress'><img src='Assets/Icons/email.png' height='100%'></label>" +
+                    "<input type='email' id='emailAddress' name='emailAddress' required placeholder='email address'>" +
                 "</div>" +
-                "<div class='password-box' style='height:30px'>" +
-                    "<label for='password' style='width:30px'><img src='Assets/Icons/password.png' height='100%'></label>" +
-                    "<input type='password' id='password' name='password'" +
+                "<div class='password-box'>" +
+                    "<label for='password'><img src='Assets/Icons/password.png' height='100%'></label>" +
+                    "<input type='password' id='password' name='password' required placeholder='password'>" +
+                "</div>" +
+                "<div class='login-button-box'>" +
+                    "<button id='login-button' type='submit'><b>Login</b></button>" +
                 "</div>" +
             "</div>";
+    }
+
+    doLogin() {
+        let responsePromise = this.loginFetch();
+        responsePromise.then((response) => {
+            if (response.ok) {
+                response.json().then(body => {
+                    window.localStorage.setItem('sodalisToken', body.token);
+                    let url = window.location.href.substring(0, -6);
+                    window.location.href = url;
+                });
+            } else {
+                if (document.getElementsByClassName('bad-login-alert').length === 0) {
+                    let alertElement = document.createElement('div');
+                    alertElement.className = 'bad-login-alert';
+                    alertElement.innerHTML = "Invalid credentials. Please try again.";
+                }
+            }
+        });
+    }
+
+    loginFetch() {
+        let url = "https://localhost:5001/api/authentication/login";
+        let emailNode = document.getElementById('emailAddress');
+        let passwordNode = document.getElementById('password');
+
+        let payload = {
+            'emailAddress':emailNode.value,
+            'password':passwordNode.value
+        };
+
+        fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(payload)
+        }).then((response) => response.json())
+            .then((data) => console.log('success: ', data))
+            .catch((error) => console.log('error: ', error));
     }
 }
 
